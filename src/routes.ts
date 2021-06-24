adb582e050013a96a6c9277242bae63496e2ea8d
gosilvapro <gabriel.oliveira@ambientesolido.com>
2021-06-24 17:33:15 -0300

    upando jwt

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,8 @@ import {Router} from 'express'
 import {CreateUserController} from "./controllers/CreateUserController";
 import { CreateTagController } from './controllers/CreateTagController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateComplimentController } from './controllers/CreateComplimentController';
 
 const router = Router();
 
@@ -11,5 +13,11 @@ router.post("/users", createUsersController.handle)
 
 const createTagController = new CreateTagController();
 router.post("/tags",ensureAdmin, createTagController.handle)
+
+const authenticateUserController = new AuthenticateUserController()
+router.post("/login", authenticateUserController.handle)
+
+const createComplimentController = new CreateComplimentController();
+router.post("/compliments", createComplimentController.handle)
 
 export {router};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,8 @@
 import { getCustomRepository } from "typeorm";
 import { compare } from "bcryptjs";
+import { sign} from "jsonwebtoken"
 import { UsersRepositories } from "../repositories/UsersRepositories";
+import { Subject } from "typeorm/persistence/Subject";
 
 interface IAutheticateRequest {
     email: string;
@@ -17,7 +19,19 @@ class AuthenticateUserService {
         if (!user) {
             throw new Error("Email/Password incorrect");
         }
+        const passMatch = await compare(password, user.password)
 
+        if (!passMatch) {
+            throw new Error("Email/Password incorrect");
+        }
+
+        const token = sign({
+            email: user.email
+        },  "647431b5ca55b04fdf3c2fce31ef1915", 
+        {
+            subject : user.id,
+            expiresIn: "1d"
+        });
     }
 }
 
